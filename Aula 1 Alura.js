097f010a0dc684e9118d1f3107ed428fbe1beb35
Pedro Moura <pedrohenriquemoura011@gmail.com>
2024-11-18 21:21:45 -0300

    Add files via upload

---
+++ Aula 1 Alura.js
@@ -0,0 +1,15 @@
+alert ("Boas vindas ao nosso site!");
+    let nome = Lua;
+        let idade = 25;
+            let numeroDeVendas = 50;
+                let saldoDisponível = 1000;
+                    alert ("Erro! Preencha todos os campos");
+                        let  mensagemDeErro("Erro! Preencha todos os campos");
+                            alert("mensagemDeErro");
+                                let nome = prompt ("Digite seu nome");
+                                    let idade = prompt ("Digite sua idade");
+                                        let idade = prompt ("Digite sua idade");
+                                            if(idade>=18) {
+                                                alert ("Pode tirar Habilitação!")   
+
+                                            }
